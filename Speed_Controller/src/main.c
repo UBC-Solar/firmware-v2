d984709adf5d0f1655f0f65e1b45971f07eedaad
Nic Ricci <teamriot10@gmail.com>
2019-10-19 12:53:33 -0700

    Init commit for Regen breaks

--- Speed_Controller/src/main.c
+++ Speed_Controller/src/main.c
@@ -9,6 +9,11 @@
 
 #define DRIVE_CONTROL_ID 0x400
 #define BATTERY_FULL_MSG 0x622
+#define ADC_MAX 255					// TODO: find out what the actual ADC_MAX is
+
+#define REGENTOGGLE 1
+#define TRUE 1
+#define FALSE 0
 
 union {
 	float float_var;
@@ -20,38 +25,106 @@ union {
 	uint8_t chars[4];
 } v;
 
-int main(void){
-	
-	CANInit(CAN_500KBPS);
-	EncoderInit();
-	TimerInit(200);
-	VirtualComInit();
+
+void sendDriveCommand(void)
+{	
 	CAN_msg_t CAN_drive;     
 	
+	volatile uint16_t encoder_reading;
+	volatile uint16_t adc_reading;
+
 	CAN_drive.len = 8;
 	
 	CAN_drive.id = DRIVE_CONTROL_ID + 1;
-	
+
 	v.float_var = 100.0;
-	u.float_var = 0.0;
 	
+
 	//set velocity to 100 for drive messages
 	CAN_drive.data[0] = v.chars[0];
 	CAN_drive.data[1] = v.chars[1];
 	CAN_drive.data[2] = v.chars[2];
 	CAN_drive.data[3] = v.chars[3];
 	
-	//set initial 
+
+	encoder_reading = EncoderRead();
+
+	//Use a parabolic scaling
+	//u.float_var = (float)(2*((float) encoder_reading/PEDAL_MAX) - ((float) encoder_reading/PEDAL_MAX)*((float) encoder_reading/PEDAL_MAX));		
+	u.float_var = (float) ((float) encoder_reading/PEDAL_MAX);
+		
 	CAN_drive.data[4] = u.chars[0];
 	CAN_drive.data[5] = u.chars[1];
 	CAN_drive.data[6] = u.chars[2];
 	CAN_drive.data[7] = u.chars[3];
 	
-	volatile uint16_t encoder_reading;
-	uint16_t old_encoder_reading = 0x0000;
+	CANSend(&CAN_drive);
+	//SendInt(DRIVE_CONTROL_ID + 1);
+	//SendLine();
+
+	RestartTimer();
+}
+
+void sendRegenCommand(void)
+{	
+	CAN_msg_t CAN_drive;     
+	
+	volatile uint16_t adc_reading;
+	adc_reading = ReadADC();
+
+	CAN_drive.len = 8;
+	
+	CAN_drive.id = DRIVE_CONTROL_ID + 1;
+
+	v.float_var = 0.0;
+
+	//set velocity to 0 for regen messages
+	CAN_drive.data[0] = v.chars[0];
+	CAN_drive.data[1] = v.chars[1];
+	CAN_drive.data[2] = v.chars[2];
+	CAN_drive.data[3] = v.chars[3];
+
+	//Use a parabolic scaling
+	//u.float_var = (float)(2*((float) encoder_reading/PEDAL_MAX) - ((float) encoder_reading/PEDAL_MAX)*((float) encoder_reading/PEDAL_MAX));		
+	u.float_var = (float) adc_reading/ADC_MAX;
+		
+	CAN_drive.data[4] = u.chars[0];
+	CAN_drive.data[5] = u.chars[1];
+	CAN_drive.data[6] = u.chars[2];
+	CAN_drive.data[7] = u.chars[3];
+	
+	CANSend(&CAN_drive);
+	//SendInt(DRIVE_CONTROL_ID + 1);
+	//SendLine();
+
+	RestartTimer();
+}
+
+int main(void){
+	
+	CANInit(CAN_500KBPS);
+	EncoderInit();
+	ADCInit();
+	TimerInit(200);
+	VirtualComInit();
 	
 	CAN_msg_t testCAN;
-	
+	CAN_msg_t CAN_drive;
+
+	volatile uint16_t encoder_reading;
+	volatile uint16_t adc_reading;
+
+	uint16_t old_encoder_reading = 0x0000;
+
+	u.float_var = 0.0;
+	//set initial values to current
+	CAN_drive.data[4] = u.chars[0];
+	CAN_drive.data[5] = u.chars[1];
+	CAN_drive.data[6] = u.chars[2];
+	CAN_drive.data[7] = u.chars[3];
+
+	encoder_reading = EncoderRead();
+	adc_reading = ReadADC();
 	while(1) 
 	{
 		
@@ -71,37 +144,27 @@ int main(void){
 		}
 		*/
 		
-		
-		
-		encoder_reading = EncoderRead();
-		
 		//SendInt(encoder_reading);
 		//SendLine();
 		
+		if(REGENTOGGLE) {
+			if(adc_reading != 0) {
+				sendRegenCommand();
+			}
+			else{
+				sendDriveCommand();
+			}
 		
-		//If the encoder count changed, send new drive CAN message and restart timer
-		if(old_encoder_reading != encoder_reading)
-		{
-
-			
-			//Use a parabolic scaling
-			//u.float_var = (float)(2*((float) encoder_reading/PEDAL_MAX) - ((float) encoder_reading/PEDAL_MAX)*((float) encoder_reading/PEDAL_MAX));		
-			u.float_var = (float) ((float) encoder_reading/PEDAL_MAX);
-				
-			CAN_drive.data[4] = u.chars[0];
-			CAN_drive.data[5] = u.chars[1];
-			CAN_drive.data[6] = u.chars[2];
-			CAN_drive.data[7] = u.chars[3];
-			
-			CANSend(&CAN_drive);
-			//SendInt(DRIVE_CONTROL_ID + 1);
-			//SendLine();
-		
-			RestartTimer();
-		
-		}		
+		}
+		else {
+				//If the encoder count changed, send new drive CAN message and restart timer
+				if(old_encoder_reading != encoder_reading)
+				{
+					sendDriveCommand();
+				}
+			}
 		//If a timeout occured, send the previously sent CAN drive message
-		else if(timeoutFlag == TRUE)
+		if(timeoutFlag == TRUE)
 		{	
 			
 			CANSend (&CAN_drive);
