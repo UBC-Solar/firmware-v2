d037d6a52265795c6ec0974c88cc4ea55ebd57f6
unknown <teamriot10@gmail.com>
2019-10-25 11:04:59 -0700

    Merged sendRegenCommand & sendDriveCommand into sendMotorCommand. Also did some clean up.

--- Speed_Controller/src/main.c
+++ Speed_Controller/src/main.c
@@ -13,7 +13,7 @@
 
 #define ADC_MAX 255					// TODO: find out what the actual ADC_MAX is
 
-#define REGENTOGGLE 1
+#define REGENTOGGLE 1				// TODO: Add input for REGENTOGGLE
 #define TRUE 1
 #define FALSE 0
 
@@ -28,81 +28,30 @@ union {
 } v;
 
 
-void sendDriveCommand(void)
-{	
-	CAN_msg_t CAN_drive;     
+void sendMotorCommand(CAN_msg_t CAN, float curr, float vel)
+{
+	v.float_var = vel;
+	u.float_var = curr;
 	
-	volatile uint16_t encoder_reading;
-	volatile uint16_t adc_reading;
-
-	CAN_drive.len = 8;
+	//Set current
+	CAN.data[4] = u.chars[0];
+	CAN.data[5] = u.chars[1];
+	CAN.data[6] = u.chars[2];
+	CAN.data[7] = u.chars[3];
 	
-	CAN_drive.id = DRIVE_CONTROL_ID + 1;
-
-	v.float_var = 100.0;
+	//set velocity
+	CAN.data[0] = v.chars[0];
+	CAN.data[1] = v.chars[1];
+	CAN.data[2] = v.chars[2];
+	CAN.data[3] = v.chars[3];
 	
-
-	//set velocity to 100 for drive messages
-	CAN_drive.data[0] = v.chars[0];
-	CAN_drive.data[1] = v.chars[1];
-	CAN_drive.data[2] = v.chars[2];
-	CAN_drive.data[3] = v.chars[3];
+	CANSend(&CAN);
 	
-
-	encoder_reading = EncoderRead();
-
-	//Use a parabolic scaling
-	//u.float_var = (float)(2*((float) encoder_reading/PEDAL_MAX) - ((float) encoder_reading/PEDAL_MAX)*((float) encoder_reading/PEDAL_MAX));		
-	u.float_var = (float) ((float) encoder_reading/PEDAL_MAX);
-		
-	CAN_drive.data[4] = u.chars[0];
-	CAN_drive.data[5] = u.chars[1];
-	CAN_drive.data[6] = u.chars[2];
-	CAN_drive.data[7] = u.chars[3];
-	
-	CANSend(&CAN_drive);
-	//SendInt(DRIVE_CONTROL_ID + 1);
-	//SendLine();
-
 	RestartTimer();
 }
 
-void sendRegenCommand(void)
-{	
-	CAN_msg_t CAN_drive;     
-	
-	volatile uint16_t adc_reading;
-	adc_reading = ReadADC();
-
-	CAN_drive.len = 8;
-	
-	CAN_drive.id = DRIVE_CONTROL_ID + 1;
-
-	v.float_var = 0.0;
-
-	//set velocity to 0 for regen messages
-	CAN_drive.data[0] = v.chars[0];
-	CAN_drive.data[1] = v.chars[1];
-	CAN_drive.data[2] = v.chars[2];
-	CAN_drive.data[3] = v.chars[3];
-
-	//Use a parabolic scaling
-	//u.float_var = (float)(2*((float) encoder_reading/PEDAL_MAX) - ((float) encoder_reading/PEDAL_MAX)*((float) encoder_reading/PEDAL_MAX));		
-	u.float_var = (float) adc_reading/ADC_MAX;
-		
-	CAN_drive.data[4] = u.chars[0];
-	CAN_drive.data[5] = u.chars[1];
-	CAN_drive.data[6] = u.chars[2];
-	CAN_drive.data[7] = u.chars[3];
-	
-	CANSend(&CAN_drive);
-	//SendInt(DRIVE_CONTROL_ID + 1);
-	//SendLine();
-
-	RestartTimer();
-}
-
-int main(void){
+int main(void)
+{
 	
 	CANInit(CAN_500KBPS);
 	EncoderInit();
@@ -110,38 +59,35 @@ int main(void){
 	TimerInit(200);
 	VirtualComInit();
 	
-	CAN_msg_t testCAN;
-	CAN_msg_t CAN_drive;
-
+	//CAN recive setup
 	CAN_msg_t CAN_rx_msg;
   CAN_msg_t CAN_tx_msg;
+	
+	//CAM motor command setup
+	CAN_msg_t CAN_drive;
+	CAN_drive.len = 8;
+	CAN_drive.id = DRIVE_CONTROL_ID + 1;
 	
 	volatile uint16_t encoder_reading;
 	volatile uint16_t adc_reading;
 
 	uint16_t old_encoder_reading = 0x0000;
-	
 	uint8_t batteryPercent = 0x00;
 
-	u.float_var = 0.0;
-	//set initial values to current
-	CAN_drive.data[4] = u.chars[0];
-	CAN_drive.data[5] = u.chars[1];
-	CAN_drive.data[6] = u.chars[2];
-	CAN_drive.data[7] = u.chars[3];
-
-	encoder_reading = EncoderRead();
-	adc_reading = ReadADC();
 	while(1) 
 	{
 		
+		//Gets all new values
+		encoder_reading = EncoderRead();
+		adc_reading = ReadADC();
 		
+		//Updates the battery percentage
 		if (CANMsgAvail())
 		{
 			CANReceive(&CAN_rx_msg);
 			
 			if(CAN_rx_msg.id == BATT_BASE + 6) {
-				batteryPercent = (int8_t) CAN_rx_msg.data[0]; //Im just going to assume that this gives a percentage? 0-100?
+				batteryPercent = (int8_t) CAN_rx_msg.data[0];
 			}
 		}
 		
@@ -149,30 +95,37 @@ int main(void){
 		//SendInt(encoder_reading);
 		//SendLine();
 		
-		if(REGENTOGGLE && batteryPercent < 98) { //Checks to see if the REGEN is ON and if the batter is under 98%
-			if(adc_reading != 0) {
-				sendRegenCommand();
+		//Checks if Regen is on and battery is beloew 98%
+		if(REGENTOGGLE && batteryPercent < 98)
+		{
+			//IChecks for regen knob not at 0
+			if(adc_reading != 0) // TODO: Check for change in adc knob so we dont flood CAN, TODO: Add min for adc knob
+			{
+				//sends knob percentage and velocity
+				sendMotorCommand(CAN_drive, adc_reading/ADC_MAX, 0.000);
 			}
-			else{
-				sendDriveCommand();
+			else if(old_encoder_reading != encoder_reading) 
+			{
+				//Sends peddle percentage and velocity
+				sendMotorCommand(CAN_drive, encoder_reading/PEDAL_MAX, 100.000);
 			}
 		
-		}
-		else {
+		} else 
+		{
 				//If the encoder count changed, send new drive CAN message and restart timer
 				if(old_encoder_reading != encoder_reading)
 				{
-					sendDriveCommand();
+					sendMotorCommand(CAN_drive, encoder_reading/PEDAL_MAX, 100.000);
 				}
-			}
+		 }
+		
 		//If a timeout occured, send the previously sent CAN drive message
-		if(timeoutFlag == TRUE)
+		 if(timeoutFlag == TRUE) //TODO: I dont think this is going to work
 		{	
 			
 			CANSend (&CAN_drive);
 			//SendInt(DRIVE_CONTROL_ID + 1);
 			//SendLine();
-			
 			
 			timeoutFlag = FALSE;
 			RestartTimer();
