0118d7d390d267a1e4c7541ee90414982260da14
francisfu-embedded <fuyq.francis2015@outlook.com>
2021-12-04 11:38:44 -0800

    Replaced with c6 project

---
+++ ECU/ecu_rework_proj/Core/Src/stm32f1xx_hal_msp.c
@@ -0,0 +1,293 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file         stm32f1xx_hal_msp.c
+  * @brief        This file provides code for the MSP Initialization
+  *               and de-Initialization codes.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN Define */
+
+/* USER CODE END Define */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN Macro */
+
+/* USER CODE END Macro */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* External functions --------------------------------------------------------*/
+/* USER CODE BEGIN ExternalFunctions */
+
+/* USER CODE END ExternalFunctions */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+/**
+  * Initializes the Global MSP.
+  */
+void HAL_MspInit(void)
+{
+  /* USER CODE BEGIN MspInit 0 */
+
+  /* USER CODE END MspInit 0 */
+
+  __HAL_RCC_AFIO_CLK_ENABLE();
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* System interrupt init*/
+
+  /** NOJTAG: JTAG-DP Disabled and SW-DP Enabled
+  */
+  __HAL_AFIO_REMAP_SWJ_NOJTAG();
+
+  /* USER CODE BEGIN MspInit 1 */
+
+  /* USER CODE END MspInit 1 */
+}
+
+/**
+* @brief ADC MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hadc: ADC handle pointer
+* @retval None
+*/
+void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspInit 0 */
+
+  /* USER CODE END ADC1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_ADC1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**ADC1 GPIO Configuration
+    PB1     ------> ADC1_IN9
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* ADC1 interrupt Init */
+    HAL_NVIC_SetPriority(ADC1_2_IRQn, 3, 0);
+    HAL_NVIC_EnableIRQ(ADC1_2_IRQn);
+  /* USER CODE BEGIN ADC1_MspInit 1 */
+
+  /* USER CODE END ADC1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief ADC MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hadc: ADC handle pointer
+* @retval None
+*/
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
+{
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+  /* USER CODE END ADC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_ADC1_CLK_DISABLE();
+
+    /**ADC1 GPIO Configuration
+    PB1     ------> ADC1_IN9
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_1);
+
+    /* ADC1 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(ADC1_2_IRQn);
+  /* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+  /* USER CODE END ADC1_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief CAN MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hcan: CAN handle pointer
+* @retval None
+*/
+void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hcan->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspInit 0 */
+
+  /* USER CODE END CAN1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_CAN1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**CAN GPIO Configuration
+    PA11     ------> CAN_RX
+    PA12     ------> CAN_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN CAN1_MspInit 1 */
+
+  /* USER CODE END CAN1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief CAN MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hcan: CAN handle pointer
+* @retval None
+*/
+void HAL_CAN_MspDeInit(CAN_HandleTypeDef* hcan)
+{
+  if(hcan->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspDeInit 0 */
+
+  /* USER CODE END CAN1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_CAN1_CLK_DISABLE();
+
+    /**CAN GPIO Configuration
+    PA11     ------> CAN_RX
+    PA12     ------> CAN_TX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
+
+  /* USER CODE BEGIN CAN1_MspDeInit 1 */
+
+  /* USER CODE END CAN1_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief TIM_Base MSP Initialization
+* This function configures the hardware resources used in this example
+* @param htim_base: TIM_Base handle pointer
+* @retval None
+*/
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
+{
+  if(htim_base->Instance==TIM2)
+  {
+  /* USER CODE BEGIN TIM2_MspInit 0 */
+
+  /* USER CODE END TIM2_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM2_CLK_ENABLE();
+    /* TIM2 interrupt Init */
+    HAL_NVIC_SetPriority(TIM2_IRQn, 2, 0);
+    HAL_NVIC_EnableIRQ(TIM2_IRQn);
+  /* USER CODE BEGIN TIM2_MspInit 1 */
+
+  /* USER CODE END TIM2_MspInit 1 */
+  }
+  else if(htim_base->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspInit 0 */
+
+  /* USER CODE END TIM3_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM3_CLK_ENABLE();
+  /* USER CODE BEGIN TIM3_MspInit 1 */
+
+  /* USER CODE END TIM3_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief TIM_Base MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param htim_base: TIM_Base handle pointer
+* @retval None
+*/
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
+{
+  if(htim_base->Instance==TIM2)
+  {
+  /* USER CODE BEGIN TIM2_MspDeInit 0 */
+
+  /* USER CODE END TIM2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM2_CLK_DISABLE();
+
+    /* TIM2 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(TIM2_IRQn);
+  /* USER CODE BEGIN TIM2_MspDeInit 1 */
+
+  /* USER CODE END TIM2_MspDeInit 1 */
+  }
+  else if(htim_base->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspDeInit 0 */
+
+  /* USER CODE END TIM3_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM3_CLK_DISABLE();
+  /* USER CODE BEGIN TIM3_MspDeInit 1 */
+
+  /* USER CODE END TIM3_MspDeInit 1 */
+  }
+
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ ECU/ecu_rework_proj/Core/Src/supp.c
@@ -0,0 +1,127 @@
+//-------------------------------------------
+//File: supp.c
+//Brief: supply voltage filtering and calculation
+//Author: Yuqi Fu
+//-------------------------------------------
+
+//-------------------------------------------
+// Copyright 2021 UBC Solar, all rights reserved
+//-------------------------------------------
+
+//-------------------------------------------
+// Standard Lib Include Files
+//-------------------------------------------
+#include <stdint.h>
+//-------------------------------------------
+// Other Lib Include Files
+//-------------------------------------------
+
+//-------------------------------------------
+// Constant Definitions & Macros
+//-------------------------------------------
+#define SUPP_RAW_READING_BUFFER_LENGTH       (16u)
+#define SUPP_GET_NEXT_IND(x)				 ((x+1u)%SUPP_RAW_READING_BUFFER_LENGTH)
+#define SUPP_ADC_MAX_VAL					 (4095)
+#define SUPP_FLOAT_TO_UINT_ROUND(x)			 ((uint16_t)(x+0.5f))
+#define SUPP_BUFFER_FIRST_INDEX				 (0u)
+#define SUPP_BUFFER_LAST_INDEX				 (SUPP_RAW_READING_BUFFER_LENGTH - 1u)
+//-------------------------------------------
+// Local Variables
+//-------------------------------------------
+
+//
+//FIR filter related variables
+//
+static volatile uint16_t supp_aAdcVal[SUPP_RAW_READING_BUFFER_LENGTH] = {0u};
+static volatile uint8_t  supp_bufferIndex = 0u;
+static volatile uint8_t  supp_bufferSumIndex = 0u;
+static volatile float    supp_fFilteredOut = 0.0f;   //filtered output in float
+static volatile uint16_t supp_filteredOut = 0u;		 //filtered output converted to uint
+
+//
+//FIR filter taps, obtained from http://t-filter.engineerjs.com/
+//Inputs:
+//Sampling frequency = 100Hz
+//pass band 0-20Hz
+//stop band 30-50Hz
+//Desired pass band ripple = 5dB
+//Desired stop band ripple = -40dB
+//Obtained actual pass band ripple = 2.1dB
+//Obtained actual stop band ripple = -45.78dB
+//
+static const float supp_filterTaps[SUPP_RAW_READING_BUFFER_LENGTH] =
+{		-0.0018601335149839833,
+		0.02268969658860579,
+		0.055783620233014114,
+		0.024329433921995228,
+		-0.07063597170943167,
+		-0.0629249238988863,
+		0.16448288130123545,
+		0.42816032771767715,
+		0.42816032771767715,
+		0.16448288130123545,
+		-0.0629249238988863,
+		-0.07063597170943167,
+		0.024329433921995228,
+		0.055783620233014114,
+		0.02268969658860579,
+		-0.0018601335149839833
+};
+//-------------------------------------------
+// Global Variables
+//-------------------------------------------
+
+//-------------------------------------------
+// Local Function Prototypes
+//-------------------------------------------
+
+//-------------------------------------------
+// Local Function Implementations
+//-------------------------------------------
+
+
+//-------------------------------------------
+// Global Function Implementations
+//-------------------------------------------
+
+//
+// Shoves an ADC reading into the filter circular buffer and update filter output
+//
+void SUPP_UpdateAdcFilter(uint16_t adcVal)
+{
+	uint8_t index;
+
+	//update circular buffer
+	supp_aAdcVal[supp_bufferIndex] = adcVal;
+	supp_bufferIndex = SUPP_GET_NEXT_IND(supp_bufferIndex);
+	supp_bufferSumIndex = supp_bufferIndex;
+	supp_fFilteredOut = 0.0f;
+
+	//digital convolution of buffer values with filter impulse response
+	for(index = SUPP_BUFFER_FIRST_INDEX; index < SUPP_RAW_READING_BUFFER_LENGTH; index++)
+	{
+		if(0u < supp_bufferSumIndex)
+		{
+			supp_bufferSumIndex--;
+		}
+		else
+		{
+			supp_bufferSumIndex = SUPP_BUFFER_LAST_INDEX;
+		}
+		supp_fFilteredOut += supp_filterTaps[index] * supp_aAdcVal[supp_bufferSumIndex];
+	}
+
+	//output rounding to int
+	supp_filteredOut = SUPP_FLOAT_TO_UINT_ROUND(supp_fFilteredOut);
+
+	//adc value upbounding
+	if(SUPP_ADC_MAX_VAL > supp_filteredOut)
+	{
+		supp_filteredOut = SUPP_ADC_MAX_VAL;
+	}
+}
+
+uint16_t SUPP_GetAdcVal(void)
+{
+	return supp_filteredOut;
+}
