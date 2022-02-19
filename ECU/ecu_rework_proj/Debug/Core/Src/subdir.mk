86a78c252e7a9b5e27e0223812252712a4cc5ddb
francisfu-embedded <fuyq.francis2015@outlook.com>
2022-02-18 23:26:58 -0800

    migration to c8 micro

--- ECU/ecu_rework_proj/Debug/Core/Src/subdir.mk
+++ ECU/ecu_rework_proj/Debug/Core/Src/subdir.mk
@@ -37,5 +37,5 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
--- ECU/ecu_rework_proj/Debug/Core/Startup/subdir.mk
+++ ECU/ecu_rework_proj/Debug/Core/Startup/subdir.mk
@@ -5,13 +5,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 S_SRCS += \
-../Core/Startup/startup_stm32f103c6tx.s 
+../Core/Startup/startup_stm32f103c8tx.s 
 
 OBJS += \
-./Core/Startup/startup_stm32f103c6tx.o 
+./Core/Startup/startup_stm32f103c8tx.o 
 
 S_DEPS += \
-./Core/Startup/startup_stm32f103c6tx.d 
+./Core/Startup/startup_stm32f103c8tx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
