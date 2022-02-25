b271f02c4a6180e73646fc2a8e05062e5f859712
a2k-hanlon <andrewkhanlon@gmail.com>
2022-02-25 01:25:21 -0800

    Clean up ADC driver in Speed Controller

--- Speed_Controller/src/ADC/ADC.c
+++ Speed_Controller/src/ADC/ADC.c
@@ -1,57 +1,66 @@
 #include "ADC.h"
+#if ADC_DEBUG
 #include "virtual_com.h"
+#endif /* ADC_DEBUG */
 
 // 0 - 300 hex pot
 
-/** 
- * Initializes an ADC for the regenerative braking toggle
+/**
+ * Initializes an ADC for the regenerative braking control
  */
-void ADCInit(void){
-	
-	// Init PA0 to analog input (0x0)
-	GPIOA->CRL &= ~(0xFUL);
-	
+void ADCInit(void)
+{
+	// Init PA0 to analog input (CNF and MODE bits all 0)
+	GPIOA->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0);
+
 	// Configure ADC2
 	// Enable ADC clock
 	RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
-	
-	// Enable config registers
+
+	// Set config registers
 	// By default, CR1 is set correctly
+
+	// Enable continuous conversion mode
 	ADC2->CR2 |= ADC_CR2_CONT;
-	
-	ADC2->SMPR2 |= 0x7UL;
-	
-	// Start ADC
-	ADC2->CR2 |= ADC_CR2_ADON;
-	ADC2->CR2    |= 0x8UL;
-	while ((ADC2->CR2 >> 3) & 0x1UL);
-	ADC2->CR2    |= 0x4UL;
-	while ((ADC2->CR2 >> 2) & 0x1UL);
-	ADC2->CR2 |= 0x1UL;
-	
+
+	// Set ADC2 Channel 0 to longest sampling time (239.5 cycles)
+	ADC2->SMPR2 |= 0x7UL << ADC_SMPR2_SMP0_Pos;
+
+	ADC2->CR2 |= ADC_CR2_ADON;	 		// Power up ADC
+	ADC2->CR2 |= ADC_CR2_RSTCAL; 		// Reset ADC calibration
+	while (ADC2->CR2 & ADC_CR2_RSTCAL);	// Wait for completion of calibration reset
+	ADC2->CR2 |= ADC_CR2_CAL; 			// Initialize ADC calibration
+	while (ADC2->CR2 & ADC_CR2_CAL);	// Wait for completion of calibration
+	ADC2->CR2 |= ADC_CR2_ADON; 			// Start ADC Conversion
 }
 
 /**
  * Reads a value from the ADC
+ * 
+ * If ADC_DEBUG is on (true), prints the reading to the virtual COM as well
+ * If ADC_REVERSE_READING is on (true), the reading is negated and offset by ADC_ZERO_THRESHOLD
+ * 
+ * @returns Either the raw ADC reading or (ADC_ZERO_THRESHOLD - raw reading) depending on ADC_REVERSE_READING 
  */
-uint16_t ReadADC(void){
-	
+uint16_t ReadADC(void)
+{
 	uint16_t ADC_reading = ADC2->DR;
-	
-	//SendString("    Raw ADC Reading:");
-	//SendInt(ADC_reading);
-	
-	/*
+
+#if ADC_DEBUG
+	SendString("    Raw ADC Reading:");
+	SendInt(ADC_reading);
+#endif /* ADC_DEBUG */
+
+#if ADC_REVERSE_READING
 	if (ADC_reading > ADC_ZERO_THRESHOLD)
 	{
-		ADC_reading = 0x000;
+		ADC_reading = 0x0;
 	}
 	else
 	{
 		ADC_reading = ADC_ZERO_THRESHOLD - ADC_reading;
 	}
-	*/
-	
+#endif /* ADC_REVERSE_READING */
+
 	return ADC_reading;
 }
-
--- Speed_Controller/src/ADC/ADC.h
+++ Speed_Controller/src/ADC/ADC.h
@@ -3,16 +3,12 @@
 
 #include "stm32f103xb.h"
 
+// Options for ReadADC()
+#define ADC_DEBUG 0
+#define ADC_REVERSE_READING 0
 #define ADC_ZERO_THRESHOLD 0x300
 
-/**
- * Initializes an ADC for the regenerative braking toggle
- */
 void ADCInit(void);
-
-/**
- * Reads a value from the ADC
- */
 uint16_t ReadADC(void);
 
 #endif /* ADC_H */
