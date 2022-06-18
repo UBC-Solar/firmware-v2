b749ad83d0fa2272e7d0680f7520fb8c587068ab
Jaynith <janithwijekoon@gmail.com>
2022-06-18 16:48:19 -0700

    EVERYTHING WORKS FINALLY!! :)

--- Speed_Controller_rewrite/Core/Src/main.c
+++ Speed_Controller_rewrite/Core/Src/main.c
@@ -61,7 +61,7 @@ typedef union
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc1;
+ ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
 CAN_HandleTypeDef hcan;
@@ -151,6 +151,7 @@ int main(void)
 
   /* Configure the system clock */
   SystemClock_Config();
+
   /* USER CODE BEGIN SysInit */
 
   /* USER CODE END SysInit */
@@ -190,8 +191,8 @@ int main(void)
 
       if (adcReadings[0] > PEDAL_OVERFLOW)
       {
-        printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : OVFLW\r\n", (int)(currentSetpoint.f*100), (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
-        SendMotorCommand((FloatBytes_t) 0.0f, velocitySetpoint);
+        printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : OVFLW\r\n", (int)adcReadings[0], (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
+        //SendMotorCommand((FloatBytes_t) 0.0f, velocitySetpoint);
       }
       else
       {
@@ -217,7 +218,7 @@ int main(void)
 
         if (adcReadings[0] > PEDAL_MIN /*&& brakePressed == GPIO_PIN_SET*/) //Accel pressed and brake NOT pressed 
         {
-          printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : ACCEL\r\n", (int)(currentSetpoint.f*100), (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
+          printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : ACCEL\r\n", (int)adcReadings[0], (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
 
           // Fix target current between 0-1
           if (currentSetpoint.f > 1.0f)
@@ -229,7 +230,7 @@ int main(void)
         } else if (adcReadings[0] <= PEDAL_MIN){ //Accel NOT pressed and regen is enabled
           if (regenEnabled == GPIO_PIN_SET)
           {
-            printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : REGEN\r\n", (int)(currentSetpoint.f*100), (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
+            printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : REGEN\r\n", (int)adcReadings[0], (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
 
             if (regenSetpoint.f > 1.0f)
               regenSetpoint.f = 1.0f;
@@ -240,7 +241,7 @@ int main(void)
           }
           else 
           {
-            printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : DECEL\r\n", (int)(currentSetpoint.f*100), (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
+            printf("PEDAL : %.3d | REGEN : %.3d | REGEN_EN : %.1d | VEL : %.1d | STATE : DECEL\r\n", (int)adcReadings[0], (int)(regenSetpoint.f*100), (int)regenEnabled, (int)velocitySetpoint.f);
 
             SendMotorCommand((FloatBytes_t) 0.0f, velocitySetpoint);
           }
@@ -326,12 +327,12 @@ static void MX_ADC1_Init(void)
   /** Common config
   */
   hadc1.Instance = ADC1;
-  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
   hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T3_TRGO;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 2;
+  hadc1.Init.NbrOfConversion = 1;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
@@ -342,15 +343,6 @@ static void MX_ADC1_Init(void)
   sConfig.Channel = ADC_CHANNEL_4;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure Regular Channel
-  */
-  sConfig.Channel = ADC_CHANNEL_0;
-  sConfig.Rank = ADC_REGULAR_RANK_2;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -419,7 +411,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 3600 - 1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 10;
+  htim3.Init.Period = 10/4;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
